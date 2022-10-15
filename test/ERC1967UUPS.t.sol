3a4b2746e401e156e2e992c44a3e8784000aa84c
philogy <philippe@dumo.net>
2022-10-15 17:05:56 -0500

    Fix test and add gas snapshot

--- test/ERC1967UUPS.t.sol
+++ test/ERC1967UUPS.t.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.15;
 
 import {Test} from "forge-std/Test.sol";
-import {HuffDeployer} from "foundry-huff/HuffDeployer.sol";
 import {MockImplementation} from "./MockImplementation.sol";
 
 /// @author philogy <https://github.com/philogy>
@@ -24,15 +23,42 @@ contract ERC1967UUPSTest is Test {
         );
     }
 
-    function testForwardCall(uint256 _a) public {
+    function testForwardCalls(uint256 _a) public {
         MockImplementation proxy = MockImplementation(_deployUUPS());
         proxy.setA(_a);
         assertEq(uint256(vm.load(address(proxy), bytes32(0x0))), _a);
+        assertEq(proxy.a(), _a);
+    }
+
+    function testBubbleUpError() public {
+        MockImplementation proxy = MockImplementation(_deployUUPS());
+        vm.expectRevert(MockImplementation.MockError.selector);
+        proxy.throwError();
     }
 
     function _deployUUPS() internal returns (address proxy) {
-        proxy = HuffDeployer.config().with_args(abi.encode(baseImpl)).deploy(
-            "ERC1967UUPS"
-        );
+        proxy = _deployHuff("src/ERC1967UUPS.huff", abi.encode(baseImpl), 0);
+    }
+
+    /// @dev Custom deploy huff method instead of `HuffDeployer` due to unknown
+    /// issues
+    function _deployHuff(
+        string memory _fp,
+        bytes memory _constructorArgs,
+        uint256 _deployValue
+    ) internal returns (address deployed) {
+        string[] memory inputs = new string[](3);
+        inputs[0] = "huffc";
+        inputs[1] = "-b";
+        inputs[2] = _fp;
+        bytes memory baseCode = vm.ffi(inputs);
+        bytes memory deployCode = bytes.concat(baseCode, _constructorArgs);
+        assembly {
+            deployed := create(
+                _deployValue,
+                add(deployCode, 0x20),
+                mload(deployCode)
+            )
+        }
     }
 }
