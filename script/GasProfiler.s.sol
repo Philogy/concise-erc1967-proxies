d5d1ecf62ec734e47395d5f68d3e0816ba3c2dce
Philippe Dumonet <philippe@dumo.net>
2022-10-19 09:13:54 +0200

    Add profiler script

---
+++ script/GasProfiler.s.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity 0.8.15;
+
+import {Script} from "forge-std/Script.sol";
+import {Test} from "forge-std/Test.sol";
+import {Vm} from "forge-std/Vm.sol";
+import {console} from "forge-std/console.sol";
+import {deployJohnsProxy, deployProxy, compile, create, appendArg} from "./OtherDeployers.sol";
+import {MorphingFactory} from "../src/MorphingFactory.sol";
+import {MockImplementation} from "../test/MockImplementation.sol";
+
+using {compile} for Vm;
+using {create, appendArg} for bytes;
+
+// taken from: https://github.com/libevm/hp2/blob/main/script/GasProfiler.s.sol
+contract GasProfiler is Script, Test {
+    address johnsProxy;
+    address libevmProxy;
+    address philogyProxy;
+
+    address impl;
+
+    function setUp() public {
+        impl = address(new MockImplementation());
+
+        johnsProxy = deployJohnsProxy(vm, impl);
+        libevmProxy = deployProxy(vm, impl, address(this));
+        philogyProxy = new MorphingFactory().deployProxy(impl, bytes32(0));
+    }
+
+    function run() public {
+        uint256 gasBefore;
+        uint256 johnGasUsed;
+        uint256 libevmGasUsed;
+        uint256 philogyGasUsed;
+
+        gasBefore = gasleft();
+        MockImplementation(johnsProxy).setA(1);
+        johnGasUsed = gasBefore - gasleft();
+
+        gasBefore = gasleft();
+        MockImplementation(libevmProxy).setA(1);
+        libevmGasUsed = gasBefore - gasleft();
+
+        gasBefore = gasleft();
+        MockImplementation(philogyProxy).setA(1);
+        philogyGasUsed = gasBefore - gasleft();
+
+        console.log("MockImplementation.setA(1) gasProfiling");
+        console.log("==============================");
+
+        console.log("Jtriley's proxy gas used: ");
+        console.log(johnGasUsed);
+        console.log("====");
+        console.log("Libevm proxy gas used: ");
+        console.log(libevmGasUsed);
+        console.log("====");
+        console.log("Philogy proxy gas used: ");
+        console.log(philogyGasUsed);
+        bytes memory philogyCode = philogyProxy.code;
+        emit log_named_bytes("philogyCode", philogyCode);
+    }
+}
---
+++ script/OtherDeployers.sol
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+// taken from: `https://github.com/libevm/hp2/blob/main/src/Deployer.sol`
+
+import {Vm} from "forge-std/Vm.sol";
+
+using {compile} for Vm;
+using {appendArg, create} for bytes;
+
+function compile(Vm vm, string memory path) returns (bytes memory) {
+    string[] memory cmd = new string[](3);
+    cmd[0] = "huffc";
+    cmd[1] = "--bytecode";
+    cmd[2] = path;
+    return vm.ffi(cmd);
+}
+
+error DeploymentFailure(bytes bytecode);
+
+function create(bytes memory bytecode, uint256 value)
+    returns (address deployedAddress)
+{
+    assembly {
+        deployedAddress := create(value, add(bytecode, 0x20), mload(bytecode))
+    }
+
+    if (deployedAddress == address(0)) revert DeploymentFailure(bytecode);
+}
+
+function create2(
+    bytes memory bytecode,
+    uint256 value,
+    bytes32 salt
+) returns (address deployedAddress) {
+    assembly {
+        deployedAddress := create2(
+            value,
+            add(bytecode, 0x20),
+            mload(bytecode),
+            salt
+        )
+    }
+
+    if (deployedAddress == address(0)) revert DeploymentFailure(bytecode);
+}
+
+function appendArg(bytes memory bytecode, address arg)
+    pure
+    returns (bytes memory)
+{
+    return bytes.concat(bytecode, abi.encode(arg));
+}
+
+contract TryDeployer {
+    address a;
+
+    constructor(bytes memory bytecode, uint256 value) {
+        a = create(bytecode, value);
+    }
+
+    function getDeployedBytecode() external view returns (bytes memory) {
+        return address(a).code;
+    }
+}
+
+function deployJohnsProxy(Vm vm, address impl) returns (address) {
+    bytes memory bc = vm.compile("huff/MVP.huff").appendArg(impl);
+    return create(bc, 0);
+}
+
+function deployProxy(
+    Vm vm,
+    address impl,
+    address owner
+) returns (address) {
+    // Get this value from script/GetOffset.s.sol
+    uint256 immutableAddressOffset = 44;
+
+    bytes memory bc = vm.compile("huff/Proxy.huff").appendArg(impl).appendArg(
+        owner
+    );
+    bc[immutableAddressOffset] = bytes1(
+        0x20 + uint8(bc[immutableAddressOffset])
+    );
+    return create(bc, 0);
+}
